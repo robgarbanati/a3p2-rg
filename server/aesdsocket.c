581d8ed1a5b09d42257d6276689d80c9f6151e34
rob <Rob Garbanati>
2026-07-02 18:40:56 -0400

    set deamon-mode full-test passes

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define SERVER_PORT 9000
@@ -20,7 +21,8 @@ static void signal_handler(int sig) {
     caught_signal = 1;
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    int daemon_mode = (argc == 2 && strcmp(argv[1], "-d") == 0);
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = signal_handler;
@@ -47,6 +49,19 @@ int main(void) {
         perror("bind");
         close(server_socket_fd);
         return -1;
+    }
+
+    if (daemon_mode) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            close(server_socket_fd);
+            return -1;
+        }
+        if (pid > 0)
+            return 0;  // parent exits, shell gets control back
+        // child continues as daemon
+        setsid();
     }
 
     if (listen(server_socket_fd, 5) == -1) {
